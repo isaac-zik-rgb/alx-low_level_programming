a35d3b099a2ee485870f16483186d12fdcca8024
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-08 04:16:20 -0800

    my first stdo

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char strg = "Programming is like building a mutilingual puzzle"
+puts(strg);
+return (0);
+}
