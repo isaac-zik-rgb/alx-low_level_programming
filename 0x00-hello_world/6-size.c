65e51c59a8866359073052abf3ffa36dfa957caa
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-08 14:01:19 -0800

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@ printf("Size of a char: %d", sizeof(char));
 printf("byte(s)\n");
 printf("Size of an int: %d", sizeof(int));
 printf("byte(s)\n");
-printf("Size of a long int: %d", sizeof(int));
+printf("Size of a long int: %d", sizeof(long int));
+printf("byte(s)\n");
 printf("Size of a long long int: %d", sizeof(long long int));
 printf("byte(s)\n");
 printf("Size of a float: %d", sizeof(float));
-printf("byte(s)");
+printf("byte(s)\n");
 return (0);
 }
