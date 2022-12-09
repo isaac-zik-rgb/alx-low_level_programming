802675b02472615f53a30cf788ed39d14f2ded46
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-08 17:08:08 -0800

    set

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\"- Dora  Korpa, 2015-10-19", 59);
-return (2); 
+write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19", 59);
+return (2);
 }
