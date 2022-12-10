545e323a6c6ad9f0a77c422bece54644eae08525
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-10 15:14:43 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ n = rand() - RAND_MAX / 2;
 i = n % 10;
 printf("Last digit of %d is %d ", n, i);
 if (i > 5)
-printf("and is greater than 5");
+printf("and is greater than 5\n");
 else if (i == 0)
-printf("and is 0");
+printf("and is 0\n");
 else if ((i < 6) != 0)
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 return (0);
 }
