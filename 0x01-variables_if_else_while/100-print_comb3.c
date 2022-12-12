92b77b6d5bd13aafbb6a648cb37de81f8561142a
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-11 17:49:43 -0800

    digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 int i;
 int a;
-for (i = 48; i <= 57; i++)
+for (i = 48; i <= 56; i++)
 {
-for (a = 49; a <= 56; a++)
+for (a = 49; a <= 57; a++)
 {
 if (a > i)
 {
 putchar(i);
 putchar(a);
-if (i != 57 || a != 56)
+if (i != 56 || a != 57)
 {
 putchar(44);
 putchar(32);
