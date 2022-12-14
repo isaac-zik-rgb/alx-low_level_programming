2f55082fe16dc53ddb44ea4053fd7cfb6e975979
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-14 15:19:24 -0800

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - A function that check for alpha characters
+ *@c: Returns an integar parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+return (c >= 65 && c <= 90);
+}
