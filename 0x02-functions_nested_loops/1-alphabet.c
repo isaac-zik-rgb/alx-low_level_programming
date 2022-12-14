9966111d05f02a8c1a2b80d15557eb16a9e45616
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-14 09:13:46 -0800

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,10 @@
  * Return: Always 0 (success)
  */
 int main(void)
-void print_alphabate(void)
 {
-  int i;
+void print_alphabate(void) = int i;
 for (i = 97; i <= 122; i++)
 _putchar(i);
  _putchar(10);
  return (0);
-} 
+}
