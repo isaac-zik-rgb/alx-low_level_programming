6870d91b1111f0cb38f351e826845f6be20b45f2
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-14 13:40:00 -0800

    don

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-  int i, x;
-  x =0;
-  while (x < 10)
-    {
-      i = 97;
-      while(i <= 122)
-	{
-	  _putchar(i);
-	  i++;
-	}
-      _putchar(10);
-      x++;
-    }
+int i, x;
+x = 0;
+while (x < 10)
+{
+i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar(10);
+x++;
+}
 }
