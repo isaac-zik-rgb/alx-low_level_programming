04372504b31fc4258c0776bfc20d3b406a89538a
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-14 07:32:33 -0800

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,6 @@
-void _putchar(void)
-{
-putchar(95);
-putchar(112);
-putchar(117);
-putchar(116);
-putchar(99);
-putchar(104);
-putchar(97);
-putchar(114);
-putchar(10);
-}
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void _putchar();
+
+#endif
