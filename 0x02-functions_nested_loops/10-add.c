4ffbf7ffe865e700a20e4ca2e506d4e7070e981b
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-15 15:18:18 -0800

    fixed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,12 @@
 /**
  * add - Add up two integer number
  *@a: return an integer value
- *
+ *@b: return another integer value
  * Return: Always 0 (Success)
  */
 int add(int a, int b)
 {
-  int result;
-  result = a + b;
-  return result;
+int result;
+result = a + b;
+return (result);
 }
