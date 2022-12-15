24a6317421e173d98683811a287d735792e2e86e
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-15 12:28:45 -0800

    don

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * abs - Compute an absolute value of an integer
+ *@int: return an integer
+ * Return: Always 0(Success)
+ */
+int _abs(int)
+{
+  int x;
+  x = 0;
+  if (x >= 0)
+    {
+      return (x);
+    }
+  else
+    {
+      return (x -'0');
+    }
+}
