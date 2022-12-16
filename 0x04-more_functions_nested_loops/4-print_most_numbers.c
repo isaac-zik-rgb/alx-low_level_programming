d4e937e32dcaccbf6b39a906282442c44a23aa61
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 14:44:31 -0800

    set

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,6 @@ void print_most_numbers(void)
 {
 int x;
 for (x = 48; x <= 57; x++)
-{
 if (x != 50 && x != 52)
 _putchar(x);
-_putchar('\n');
-}
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,16 @@ void more_numbers(void)
 {
   int i, a;
   i = 0;
-  while (i < 11)
+  while (i < 10)
     {
       a = 0;
       while (a <= 14)
 	{
 	  _putchar(a);
 	  a++;
-	
-      _putchar(10);
-      i++
 	}
+      _putchar(10);
+      i++;
     }
   putchar(10);
 }
