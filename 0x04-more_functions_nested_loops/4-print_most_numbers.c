509bd8ab5d80d278c4d43c790a3cb307a0c6cd7c
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 15:01:58 -0800

    sort

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,6 +9,11 @@ void print_most_numbers(void)
 {
 int x;
 for (x = 48; x <= 57; x++)
-if (x != 50 && x != 52)
-_putchar(x);
+  {
+if (x == 50 || x == 52)
+  continue;
+  }
+ else
+_putchar(x + '0');
+ _putchar(10);
 }
