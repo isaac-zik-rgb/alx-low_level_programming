d4c22e2549e83b40f3f3562ec269ea5cd7979438
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 13:15:47 -0800

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-print_most_numbers(void)
+void print_most_numbers(void)
 {
 int x;
 for (x = 48; x <= 57; x++)
+{
 if (x != 2 && x != 4)
 _putchar(x);
-_putchar(10);
+else
+putchar('\n');
+}
 }
