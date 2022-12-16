dd70827a8c576defaeae6ee8e075ae77d01abbaa
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 12:44:26 -0800

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - A funtion tha print integer numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+_putchar(i);
+_putchar('\n');
+}
