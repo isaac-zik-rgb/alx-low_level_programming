91e27af2981ddd00337609ad27cd6d911b7f3ce1
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 11:11:09 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase
+ *@c: returns a character data type
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c);
+{
+  if (c >= 'A' && c <= 'Z')
+  {
+  return (1);
+  else
+  return (0);
+  }
+  }
+  
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#This directory create all functions for loops
