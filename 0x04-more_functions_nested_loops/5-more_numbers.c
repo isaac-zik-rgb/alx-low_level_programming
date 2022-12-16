dceb950cb1587dadbf189b68df36375a0f5bdea6
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 15:46:17 -0800

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@
 void more_numbers(void)
 {
 int i, a;
- for (i = 0; i < 15; i++)
-   {
-     for (a = 0; a < 10; a++)
-       {
-	 if (i >= 10)
-	   _putchar(i / 10 + '0');
-	 _putchar(i % 10 + '0');
-       }
-     _putchar('\n');
-   }
+for (a = 0; a < 10; a++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i >= 10)
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+_putchar('\n');
+}
 }
