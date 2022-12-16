c568033fc8cabe9876e3c512d0f4802160c6e8cc
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 12:56:51 -0800

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - A function that print most numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+if (x != 2 && x != 4)
+_putchar(x);
+_putchar('\n');
+}
