1797368f68f45da0a1eae882ac5d61f95ccc684e
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-15 18:01:52 -0800

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
   for (i = n; i <= 98; i++)
     {
       if (i != 98)
-	printf("%d,", i);
+	printf("%d, ", i);
 	else
 	  printf("%d\n", i);
     }
@@ -25,7 +25,7 @@ void print_to_98(int n)
       for (i = n; i >= 98; i--)
 	{
 	  if (i != 98)
-	    printf("%d,", i);
+	    printf("%d, ", i);
 	  else
 	    printf("%d\n", i);
 	}
