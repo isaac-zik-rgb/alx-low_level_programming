80b7c3a5306d3caf73173d34589d3147211a7719
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 17:46:40 -0800

    cheked

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,30 +6,30 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 0; i <= 100; i ++)
-    {
-      if (i % 3 == 0 && i % 5 != 0)
-	{
-	  printf(" Fizz");
-	}
-      else if (i % 5 == 0 && i % 3 != 0)
-	{
-	  printf(" Buzz");
-	}
-      else if (i % 3 == 0 && i % 5 == 0)
-	{
-	  printf(" FizzBuzz");
-	}
-      else if (i == 1)
-	{
-	  printf("%d", 1);
-	}
-      else
-	{
-	  printf(" %d", i);
-	}
-    }
-  printf("\n");
-  return (0);
+int i;
+for (i = 0; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", 1);
+}
+else
+{
+printf(" %d", i);
+}
+}
+printf("\n");
+return (0);
 }
