4e13a405e6fb72dceeb061d90affba3ed6acff40
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 16:08:05 -0800

    set

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,15 @@
  */
 void print_line(int n)
 {
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      int x;
-      for (x = 1; x <= n; x++)
-	_putchar('_');
-    }
-  _putchar('\n');
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int x;
+for (x = 1; x <= n; x++)
+_putchar('_');
+}
+_putchar('\n');
 }
