483190d82e52ee043cf0b3167aa1c3cb2e5363a9
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 17:39:54 -0800

    game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -A program that print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i;
+  for (i = 0; i <= 100; i ++)
+    {
+      if (i % 3 == 0 && i % 5 != 0)
+	{
+	  printf(" Fizz");
+	}
+      else if (i % 5 == 0 && i % 3 != 0)
+	{
+	  printf(" Buzz");
+	}
+      else if (i % 3 == 0 && i % 5 == 0)
+	{
+	  printf(" FizzBuzz");
+	}
+      else if (i == 1)
+	{
+	  printf("%d", 1);
+	}
+      else
+	{
+	  printf(" %d", i);
+	}
+    }
+  printf("\n");
+  return (0);
+}
