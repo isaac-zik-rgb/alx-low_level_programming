9bfb5c56c90e2dddefb97a85c28c00ed837acc16
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 16:03:13 -0800

    set

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - A funtion that print a striaght line
+ * @n: n is the number of times it should be printed
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+  if (n <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      int x;
+      for (x = 1; x <= n; x++)
+	_putchar('_');
+    }
+  _putchar('\n');
+}
