e291b6d4b9ee5b9b9d0b945eb5de9b0017a009e2
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-16 16:33:21 -0800

    save

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,17 @@
  */
 void print_diagonal(int n)
 {
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      int i;
-      for (i = 1; i <= n; i++)
-	{
-	  _putchar('\\');
-	}
-      _putchar('\n');
-    }
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('\\');
+}
+_putchar('\n');
+}
 }
