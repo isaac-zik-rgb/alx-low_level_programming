284056604e0defe8f07768437f538251c4d263f2
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-17 16:42:12 -0800

    my first debuging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Testing if the function positive-or_nrgative will print
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int  positive_or_negative(0);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+int positive_or_negative(int n);
+#endif
