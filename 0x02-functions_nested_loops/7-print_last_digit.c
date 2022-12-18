b4bb83ad8df13c10dbaff0fa3b8743e24429abb0
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-17 17:19:42 -0800

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,14 @@
 int print_last_digit(int x)
 {
   int i;
+  if (x < 0)
+    
+      x = -x;
+    
   i = x % 10;
-  _putchar(i + '0');
-  return(i);
+  if (i < 0)
+    
+      i = -i;
+      _putchar(a + '0');
+      return(i);
 }
