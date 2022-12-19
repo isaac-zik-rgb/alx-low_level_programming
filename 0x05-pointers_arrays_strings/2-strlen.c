c97bfe68f9d3b657dcf3ab3379865c19e9d73975
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-19 11:17:39 -0800

    reste

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-#imclude <stdio.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * _strlen - function that returns character length
@@ -9,7 +9,7 @@
 int _strlen(char *s)
 {
   int count;
-  s = count;
+  *s = count;
   for (count = 0; count != '\0'; count++)
     return count;
 }
