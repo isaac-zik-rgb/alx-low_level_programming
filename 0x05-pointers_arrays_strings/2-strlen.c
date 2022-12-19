a3f15f4c9b0e136d94fc3effb5037542ed95930d
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-19 11:04:36 -0800

    set

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#imclude <stdio.h>
+#include "main.h"
+/**
+ * _strlen - function that returns character length
+ *@s: a char data type is passed as a parameter
+ *
+ * Return: The length of a character string
+ */
+int _strlen(char *s)
+{
+  s = length[];
+  int count;
+  for (count = 0; length[] != '\0'; count++)
+    return count;
+}
+  
