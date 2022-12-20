a721582a95a8ea46b3127e88d1e6654fb47aa4a6
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-20 02:47:41 -0800

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
  */
 void print_rev(char *s)
 {
-  int  c;
-  c = 0;
-  while (s[c] != '\0')
-    {
-      c++;
-	}
-  for (c -= 1; c >= 0; c --)
-    {
-      _putchar(s[c]);
-    }
-  _putchar('\n');
+int  c;
+c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+for (c -= 1; c >= 0; c --)
+{
+_putchar(s[c]);
+}
+_putchar('\n');
 }
