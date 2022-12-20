a61c652e327003a31499ab9228fb19a652564ff2
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-20 14:57:28 -0800

    putc

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,21 +2,21 @@
 #include "main.h"
 /**
  * puts2 - Function that print all character strings
- * @s: A character data type is passes as a parameter
+ * @str: A character data type is passes as a parameter
  *
  * Return: Always 0 success
  */
 void puts2(char *str)
 {
-  int i, c;
-  c = 0;
-  while (str[c] != '\0')
-    {
-      c++;
-    }
-  for (i = 0; i < c; i += 2)
-    {
-      _putchar(str[i]);
-    }
-  _putchar('\n');
+int i, c;
+c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+for (i = 0; i < c; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
