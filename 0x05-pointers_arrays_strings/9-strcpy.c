11fe94141fadcdb596b9524a2f4342c378646d57
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-20 17:13:35 -0800

    done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies an array character to another
+ * @
+ * @
+ * Return: Always 0 success
+ */
+char *_strcpy(char *dest, char *src)
+{
+  int i;
+  for (i = 0, scr[i] != '\0'; i++)
+    {
+      dest[i] = scr[i];
+      return (dest);
+    }
+}
