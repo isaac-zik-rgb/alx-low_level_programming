bfca0a8cccdf5479ccb770e7707b14a3d9c25cd2
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-20 17:27:03 -0800

    fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,17 +2,17 @@
 #include "main.h"
 /**
  * _strcpy - copies an array character to another
- * @
- * @
+ * @dest: Return the copied array
+ * @src: holds the character arrays
  * Return: Always 0 success
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i;
-  for (i = 0; src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  dest[i++] = '\0';
-  return(dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
