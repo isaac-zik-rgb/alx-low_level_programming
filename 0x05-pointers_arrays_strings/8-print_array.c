8175ca0e1f49ae39a5b0f2a15a8de14f8e0b2a82
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-20 16:40:35 -0800

    set

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - a function that prints array
+ *@a:  have a character pointer
+ *@n: returns an integer
+ * Return: Always 0 success
+ */
+void print_array(int *a, int n)
+{
+  int i;
+  a = &n;
+  for (i = 0; i != '\0'; i++);
+  printf("%d, ", *a(n + i));
+    printf("\n");
+}
