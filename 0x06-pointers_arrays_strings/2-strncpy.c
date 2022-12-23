034e3b119b3143d6bc97f7f946d9e1ae39d616d8
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-22 17:05:39 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int m;
-  m = 0;
-  while (m < n && src[m] != '\0')
-    {
-      dest[m] = src[m];
-      m++;
-    }
-  while (m < n)
-    {
-      dest[m] = '\0';
-      m++;
-    }
-  return dest;
+int m;
+m = 0;
+while (m < n && src[m] != '\0')
+{
+dest[m] = src[m];
+m++;
+}
+while (m < n)
+{
+dest[m] = '\0';
+m++;
+}
+return (dest);
 }
 
