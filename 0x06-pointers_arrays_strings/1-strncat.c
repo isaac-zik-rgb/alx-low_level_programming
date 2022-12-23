178f3081dbfb814d9107e66907e4a2067cf6aea8
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-22 16:20:26 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i, x;
-  i = 0;
-  x = 0;
-  while (dest[i] != '\0')
-  {
-      i++;
-  }
-  while (x < n && src[x] != '\0')
-  {
-      dest[i] = src[x];
-      i++;
-      x++;
-  }
-  dest[i] = '\0';
-      return dest;
+int i, x;
+i = 0;
+x = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (x < n && src[x] != '\0')
+{
+dest[i] = src[x];
+i++;
+x++;
+}
+dest[i] = '\0';
+return (dest);
 }
