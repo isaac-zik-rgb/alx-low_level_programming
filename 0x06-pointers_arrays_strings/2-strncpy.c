edb2c50581500ce4e8f377a2fb4c280b0d038281
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-22 16:37:35 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - function that copies  strings
+ *@dest: copies to
+ *@src: copies from
+ *@n: then number of bytes copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+  int m, p;
+  p = 0;
+  while (dest[p] != '\0')
+    {
+      p++;
+    }
+  for (m = 0; m < n && src[m] != '\0'; m++)
+    {
+      dest[p] = src[m];
+    }
+  dest[p] = '\0';
+  return dest;
+}
+
