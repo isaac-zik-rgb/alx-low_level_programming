d66933f1f92a589b381adb2878157249f1630e06
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-23 14:57:44 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * string_toupper - convert all lowercase character to uppercase
+ * string_toupper - convert all lowercase41;320;0c character to uppercase
  *
  * Return: a string pointer
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-  int i, ziko, str;
-  i = 0;
+  int  ziko, i;
+  i  = 0;
   ziko = 'a' - 'A';
-  while (*str[i])
+  while (str[i])
     {
  if ((str[i] >= 'a' && str[i] <= 'z'))
-   *str[i] = ziko;
+   str[i] = ziko;
  i++;
     }
   return (str);
