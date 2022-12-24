2dc35ba7cb265db2ee69ec350b2b547015d482f3
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-23 18:36:10 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,18 +8,18 @@
  */
 char *leet(char *s)
 {
-  int i, y;
-  char str1[] = "aAeEoOtTlL";
-  char str2[] = "443300771";
-  for (i = 0; i < 10 && s[i] != '\0'; i++)
-    {
-      for (y = 0; y < 10 && s[y] != '\0'; y++)
-	{
-	  if (s[i] == s[y])
-	    {
-	      str1[i] = str2[y];
-	    }
-	}
-    }
-  return (s);
-    }
+int i, y;
+char str1[] = "aAeEoOtTlL";
+char str2[] = "443300771";
+for (i = 0; i < 10 && s[i] != '\0'; i++)
+{
+for (y = 0; y < 10; y++)
+{
+if (s[i] == str1[y])
+{
+str1[i] = str2[y];
+}
+}
+}
+return (s);
+}
