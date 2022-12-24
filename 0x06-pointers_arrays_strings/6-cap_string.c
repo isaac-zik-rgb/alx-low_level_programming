aa6d907176114fee5bb6467ecba2b8e9fd8ca6b8
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-23 16:32:10 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - capitalizes strings
+ *@n: the strings pointer
+ *
+ * Return: n
+ */
+char *cap_string(char *n)
+{
+  int i;
+  i = 0;
+  while (n[i] != '\0')
+    {
+      if (n[i] >= 'a' && n[i] <= 'z')
+	i++;
+    }
+  if (n[i - 1] == ' ' || n[i - 1] == '\t'
+      || n[i - 1] == '\n' || n[i - 1] == ',' || n[i - 1] == ';' || n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' || n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' || n [i - 1] == '{' || n[i - 1] == '}')
+    {
+      n[i - 1] = n[i - 1] - 32;
+    }
+  return (n);
+}
