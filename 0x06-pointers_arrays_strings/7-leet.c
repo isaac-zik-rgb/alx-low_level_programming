c69f5a02f24f4cb6c9eddbe19e0ef9144ce5e2ba
isaac castro <isaacokechukwu200021@gmail.com>
2022-12-23 18:23:16 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,24 +2,24 @@
 #include "main.h"
 /**
  * leet - encodes a strings into 1337
- *@str: the encoded strings pointer
+ *@s: the encoded strings pointer
  *
  * Return: str
  */
-char *leet(char *str)
+char *leet(char *s)
 {
   int i, y;
   char str1[] = "aAeEoOtTlL";
   char str2[] = "443300771";
-  for (i = 0; i < 10 && str1[i] != '\0'; i++)
+  for (i = 0; i < 10 && s[i] != '\0'; i++)
     {
-      for (y = 0; y < 10 && str2[y] != '\0'; y++)
+      for (y = 0; y < 10 && s[y] != '\0'; y++)
 	{
-	  if (str1[i] == str2[y])
+	  if (s[i] == s[y])
 	    {
-	      str2[y] = str1[i];
+	      str1[i] = str2[y];
 	    }
 	}
     }
-  return (str);
+  return (s);
     }
