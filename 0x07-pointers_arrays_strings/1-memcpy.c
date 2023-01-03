b12428b71ca518469512729aa3daea3f9c197ed8
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-03 14:59:28 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * _memcpy - copies n bytes from memory area
+ * _memcpy - copy n bytes of memory to another
  *@dest: copy to
- *@scr: copy from
- *@n: number of bytes to be copied
- *Return: dest
+ *@src: copy from
+ *@n: number of bytes to copy to
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  int i;
-  i = 0;
-int  r = n;
-  for (; i < r; i++)
-    {
-      src[i] = src[i];
-      n--;
-    }
-  return (dest);
+int i = 0;
+int r = n;
+for (; i < r; i++)
+{
+dest[i] = src[i];
+n--;
+}
+return (dest);
 }
