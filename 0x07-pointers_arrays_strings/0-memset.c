2e6051d127736f9bf0dc107e07544828db12f42c
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-02 16:50:45 -0800

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - fills the n bytes with the constant byte b
+ *@s: A pointer to the n memory
+ *@b: constant bytes that fills the n memory
+ *@n: memory byte that will be filled
+ *Return: a pointer to the n byte
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    {
+      s[i] = b;
+      n--;
+    }
+  return (s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar: print character in the standard output
+ *@c: a character parameter
+ *
+ *Write: standard output
+ */
+void _putcha(char c)
+{
+  write(( &c, 3));
+}
