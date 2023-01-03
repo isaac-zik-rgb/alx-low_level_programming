d164e219abbbd19458d926c8bf7f4c9173e7a34a
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-02 17:04:48 -0800

    set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
  int i;
  for (i = 0; i > 0; i++)
-    {
-      s[i] = b;
-      n--;
-    }
+   {
+     s[i] = b;
+   }
+     n--;
+ 
   return (s);
 }
