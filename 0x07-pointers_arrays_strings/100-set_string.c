81109a6a4ae51e4795f6818fabd4b8792b9856a1
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-03 18:32:01 -0800

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *set_string - sets a pointer value to a character
+ *@s: a double deference/ a pointer pointer
+ *@to: point to a character
+ * Return: success
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char _putchar(char c);
 void print_diagsums(int *a, int size);
-
+void set_string(char **s, char *to);
 
 
 #endif
