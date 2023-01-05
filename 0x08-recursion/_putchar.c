bc9162836a9fb02c81eb5863c0b7bcf2be79874f
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-05 03:18:58 -0800

    st

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_putchar - print standard out-put 
+ *@c:character parameter is passed to it
+ *Return : c
+ */
+void  _putchar(char c)
+{
+  write((1, &c, 3));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+void _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+
+
+
+
+#endif
