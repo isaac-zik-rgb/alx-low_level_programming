c7579d9effe60269df5a32f436bfe834089869b2
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-05 08:39:53 -0800

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-    {
-      return;
-    }
-  else
+  if (*s)
     {
       _putchar(*s);
       _puts_recursion(s + 1);
-      return;
     }
+  else
   _putchar(10);
 }
