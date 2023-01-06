febe9760e7581eded4224241d27e04e7db8043b0
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-05 18:58:58 -0800

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,19 @@
 /**
  *_strlen_recursion - returns length of a string
  *@s: a pointer to the stirng
- *Return: pointer
+ *Return: an interger value
  */
 int _strlen_recursion(char *s)
 {
-  if (*s == '\0')
-    {
-      return 0;
-    }
-  else
-    return 1 + _strlen_recursion(s +1);
+  /*while *s point to tge first index of the array*/
+if (*s == '\0')
+{
+return (0);
+}
+/*
+ *Return: @1 + the value of the arrary
+ * using +1 to move the pointer to the nex character in the array
+*/
+else
+return (1 + _strlen_recursion(s + 1));
 }
