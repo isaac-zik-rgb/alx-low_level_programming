5a8cf59965811b164fc4debdd7a444c1db91d475
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-08 15:45:17 -0800

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: count the number of argument passed to the commanline
+ *@argv: strings passed to the command-line
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+  printf("%s\n", argv[]);
+  return (0);
+}
