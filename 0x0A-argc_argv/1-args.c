050084f9a693d4ab1dc7fdb2b7d2222298d6be09
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-08 16:43:24 -0800

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: count number of argument passed to the command-line
+ *@argv: argument vector a pointer to a strings
+ *Return: 0 success
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
