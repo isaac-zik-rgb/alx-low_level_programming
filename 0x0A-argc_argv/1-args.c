091e60992113094f6ddf0c882665b5a6badc1ac9
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-10 12:17:48 -0800

    set

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,6 @@
  */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-  int i;
-  for (i = 0; i < argc; i++)
-    printf ("%d\n", i);
-  return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
