c622759e604e77e509e1875e071a4348971d079f
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-12 17:25:54 -0800

    fic

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,18 @@ char *_strdup(char *str)
 {
   int i;
   char *strn;
+  int j = 0;
+  if (str == NULL)
+    return (NULL);
+  i = 0;
+  while (str[i] != '\0')
+    {
+      i++;
+    }
   strn = malloc(strlen(str) + 1);
   if (strn == NULL)
     return (NULL);
-  if (str == NULL)
-    return (NULL);
-  for (i = 0; str[i]; i++)
-    strn[i] = str[i];
-  strn[i] = '\0';
+  for (j = 0; str[j]; j++)
+    strn[j] = str[j];
   return (strn);
 }
