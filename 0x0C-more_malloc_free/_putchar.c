2568ed3fc3b9e24f06eb478e9f5375f1ab2f7af2
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-13 16:49:58 -0800

    fixed

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - write c on standard out-put
+ * @c: a character parameter
+ *
+ * Write: -1 on error
+ */
+int _putchar(char c)
+{
+write ((1, &c, 3));
+}
