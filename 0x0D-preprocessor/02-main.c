5f25f26c67b4e89f9d20bb1d1990b3b392615b89
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-16 16:18:09 -0800

    task2

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#define ARGV "2-main.c"
 /**
  *main - Entry point
  *@argc: argument count
- *@argv: argument vector
+@argv: argument vector
 *
 * Return: Nothing
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", __FILE__);
+  printf("%s\n", ARGV);
 return (0);
 }
