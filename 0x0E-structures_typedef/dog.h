4b14fc245abdec4bb20168de5ae4b1b39fdd51d7
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-17 12:57:54 -0800

    task1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - my struct declaration
+ * @name: a character pointer
+ * @owner: the second character pointer
+ * @age: a float number
+ */
+typedef struct dog
+{
+char *name;
+char *owner;
+float age;
+} dog;
+#endif
