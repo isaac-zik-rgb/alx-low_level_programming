640e478716667e0cf36e0ec3447f973d2730675c
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-17 19:25:50 -0800

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog: print out the value of the d
+ *@d: a pointer to the struct
+ *
+ *Return: 0 sucess
+ */
+void print_dog(struct dog *d)
+{
+  if(d)
+    {
+      d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+      printf("Age: %f\n", d->age);
+      d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n", d->owner));
+    }
+}
