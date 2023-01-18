568f688108125248d018f5dec0d40af46dde98ac
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-17 18:22:36 -0800

    set

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * _putchar - write c on standard output
+ *@c: a character parameter
+ *
+ * Write 1 if error
+ */
+int _putchar(char c)
+{
+write((1, &c, 3));
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,13 @@ char *name;
 char *owner;
 float age;
 } dog;
+/*more functions*/
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+
+
+
+
 #endif
