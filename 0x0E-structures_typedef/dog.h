162704cad32ff464ed7c34fc635abc1f13ecdb1f
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-18 16:54:36 -0800

    set

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ float age;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 
 
 
