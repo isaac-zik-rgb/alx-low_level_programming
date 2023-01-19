ab2a296919626da023b3c6e0580413c1542b8fff
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-18 17:26:58 -0800

    task1

---
+++ 0x0F-function_pointers/1-array_interator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_interator - print each array element
+ * @array: the given array
+ * @size: size of the array
+ * @action: pointer to the array to print
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array == NULL || action == NULL)
+{
+return;
+}
+for (i= 0; i < size; i++)
+{
+action(array[i]);
+}
+}
