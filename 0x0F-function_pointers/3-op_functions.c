751c4a19582a4601f871cc1e41df84d65f3b7126
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-20 03:59:41 -0800

    my function for op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "calc.h"
+/**
+ * op_add - adds up the two number pass as an argument
+ * @a: the first number
+ *@b: the second number
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtracts two number pass as an argument
+ * @a: the first argument
+ * @b: the second argument
+ *
+ *Return: the result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers passed as an argument
+ * @a: the first argument
+ * @b: the second argument
+ *
+ * Return: restult
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divide two integers passed as an argument
+ * @a: the first argument
+ * @b: the second argument
+ *
+ * Return: the result
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of two divisional number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
