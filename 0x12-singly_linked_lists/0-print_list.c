b4930c0a3f71635f744001678a98eeeb67038706
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 02:11:16 -0800

    set

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ while (h  != NULL)
 	printf("[%d]%s\n", 0, "(nil)");
       }
     else
-      printf("[%d] %s\n", h->len, h->str);
+      printf("[%u] %s\n", h->len, h->str);
     h = h->next;
     count++;
   }
