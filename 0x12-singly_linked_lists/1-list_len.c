c0994e5a4024b86a339768d7328b7f83c76b40b2
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 15:55:24 -0800

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,11 +9,11 @@
  */
 size_t list_len(const list_t *h)
 {
-  size_t count = 0;
-  while (h != NULL)
-    {
-      count++;
-      h = h->next;
-    }
-  return (count);
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
 }
