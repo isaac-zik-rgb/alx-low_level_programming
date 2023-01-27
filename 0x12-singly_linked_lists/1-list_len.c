804657fd546bfc8dd11f7badbd23ae76d807ad57
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 15:52:47 -0800

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - prints number of element in a list
+ * @h: a head pointer to the struct list
+ *
+ * Return: number of element
+ */
+size_t list_len(const list_t *h)
+{
+  size_t count = 0;
+  while (h != NULL)
+    {
+      count++;
+      h = h->next;
+    }
+  return (count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,5 +16,8 @@ struct list_s *next;
 } list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+
 
 #endif
