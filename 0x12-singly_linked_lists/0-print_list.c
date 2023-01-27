b6ee520831c0e1ada7e4dbcc51a463807e0ff3cb
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 02:19:39 -0800

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,11 +10,11 @@ size_t print_list(const list_t *h)
 {
   size_t count;
 count = 0;
-while (h  != NULL)
+while (h)
   {
-    if (h->str == NULL)
+    if (!h->str)
       {
-	printf("[%d]%s\n", 0, "(nil)");
+	printf("[0] (nil)");
       }
     else
       printf("[%u] %s\n", h->len, h->str);
