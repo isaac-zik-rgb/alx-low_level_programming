2a14912f9f3d57bec2148808b0f7d70c77ff92ef
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 15:37:33 -0800

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,21 +2,21 @@
 #include "lists.h"
 /**
  * print_list - print the elements of the node
- * @h - a singly link list
+ *@h: a head pointer to the first node of the list
  *
  * Return: numbers of nodes
  */
 size_t print_list(const list_t *h)
 {
-  unsigned int count = 0;
-  while (h)
-    {
-      if (h->str == NULL)
-	printf("[0] (nil)\n");
-      else
-	printf("[%u] %s\n", h->len, h->str);
-      h = h->next;
-      count++;
-    }
-  return (count);
+size_t count = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
 }
