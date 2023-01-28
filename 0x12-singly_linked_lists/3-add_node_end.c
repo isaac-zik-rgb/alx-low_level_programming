81d7b2477ea4cb9480cc90652c13fee99dfb57bc
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 17:55:36 -0800

    test

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - add a node at end of the list
+ * @head: a head pointer to the first node list
+ * @str: a data to be filled in the list
+ *
+ * Retrun: a pointer to the address of the last node
+ */
+list_t  *add_node_end(list_t **head, const char *str)
+{
+  unsigned int len = 0;
+  list_t *ptr, *temp;
+  ptr = (*head);
+  while (str[len])
+    len++;
+  temp = malloc(sizeof(list_t));
+  if (temp == NULL)
+    return (NULL);
+  temp->str = strdup(str);
+  temp->next = NULL;
+  while (ptr->next != NULL)
+    {
+      ptr = ptr->next;
+    }
+  ptr = temp;
+  return (temp);
+}
