1317c88aa5ac0c983e4c7a4e5a6df200d22a452c
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-27 18:14:10 -0800

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,28 +7,28 @@
  * @head: a head pointer to the first node list
  * @str: a data to be filled in the list
  *
- * Retrun: a pointer to the address of the last node
+ * Return: a pointer to the address of the last node
  */
 list_t  *add_node_end(list_t **head, const char *str)
 {
-  unsigned int len = 0;
-  list_t *ptr, *temp;
-   ptr = (*head);
-  while (str[len])
-    len++;
-  temp = malloc(sizeof(list_t));
-  if (temp == NULL)
-    return (NULL);
-  temp->str = strdup(str);
-  temp->len = len;
-  temp->next = NULL;
-  if (*head == NULL)
-    {
-      *head = temp;
-      return (temp);
-    }
-  while (ptr->next)
-    ptr = ptr->next;
-  ptr->next = temp;
-  return (temp);
+unsigned int len = 0;
+list_t *ptr, *temp;
+ptr = (*head);
+while (str[len])
+len++;
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
+return (NULL);
+temp->str = strdup(str);
+temp->len = len;
+temp->next = NULL;
+if (*head == NULL)
+{
+*head = temp;
+return (temp);
+}
+while (ptr->next)
+ptr = ptr->next;
+ptr->next = temp;
+return (temp);
 }
