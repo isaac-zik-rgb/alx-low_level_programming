f20dc60a7a790ae6f1785f2647b8c8414aaa11ca
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 01:10:16 -0800

    task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,13 +9,13 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *ptr;
-  ptr = malloc(sizeof(listint_t));
-  if (ptr == NULL)
-    return (NULL);
-  ptr->n = n;
-  ptr->next = NULL;
-  ptr->next = (*head);
- *head = ptr;
-  return (*head);
+listint_t *ptr;
+ptr = malloc(sizeof(listint_t));
+if (ptr == NULL)
+return (NULL);
+ptr->n = n;
+ptr->next = NULL;
+ptr->next = (*head);
+*head = ptr;
+return (*head);
 }
