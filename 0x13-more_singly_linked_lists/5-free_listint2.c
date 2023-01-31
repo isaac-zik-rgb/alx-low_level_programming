945ec9c760bb8b96002bafc55a59a0edb3da35d9
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 13:58:20 -0800

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,14 @@
  */
 void free_listint2(listint_t **head)
 {
-  listint_t *ptr;
-  if (head == NULL)
-    return;
-  while (*head)
-    {
-      ptr = (*head)->next;
-      free(*head);
-      *head = ptr;
-    }
-  *head =  NULL;
+listint_t *ptr;
+if (head == NULL)
+return;
+while (*head)
+{
+ptr = (*head)->next;
+free(*head);
+*head = ptr;
+}
+*head =  NULL;
 }
