f12351460cb61b3e6d26567ff90b8a7520ed7c6b
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 15:07:32 -0800

    test

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * sum_listint - get all tge sum of tge data (n) in the limked list
+ * @head: a pointer to the first node of the list
+ * Return: the sum of the n data in the node
+ */
+int sum_listint(listint_t *head)
+{
+  listint_t *temp = head;
+  int sum = 0;
+  while (temp)
+    {
+      sum += temp->n;
+      temp = temp->next;
+    }
+  return (sum);
+}
