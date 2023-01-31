3199ddf78bd86d43c0f3bc2ca82cf473815df2b3
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 15:28:42 -0800

    fixed

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,12 +7,12 @@
  */
 int sum_listint(listint_t *head)
 {
-  listint_t *temp = head;
-  int sum = 0;
-  while (temp)
-    {
-      sum += temp->n;
-      temp = temp->next;
-    }
-  return (sum);
+listint_t *temp = head;
+int sum = 0;
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
 }
