fa8101167c38e587b5dc6f773723f1701242ea20
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 13:56:04 -0800

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2- frees the memory of the lists node
+ * @head: a double pointer to the node
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+  listint_t *ptr;
+  if (head == NULL)
+    return;
+  while (*head)
+    {
+      ptr = (*head)->next;
+      free(*head);
+      *head = ptr;
+    }
+  *head =  NULL;
+}
