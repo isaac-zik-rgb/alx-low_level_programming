da0da551ca077837966bbe7b97fed96ec0da0606
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 00:12:50 -0800

    try

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *listint_len - count the numbers numbers of elements in the list
+ * @h: a pointer to tge first node of the list
+ *
+ * Return: number of element in a link list
+ */
+size_t listint_len(const listint_t *h)
+{
+  size_t count = 0;
+  while (h != NULL)
+    {
+      h = h->next;
+      count++;
+    }
+  return (count);
+}
