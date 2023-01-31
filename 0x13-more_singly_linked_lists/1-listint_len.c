adccd652f08e24c4900e5a2a00794591d75b4a47
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 00:17:44 -0800

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,11 +8,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
-  size_t count = 0;
-  while (h != NULL)
-    {
-      h = h->next;
-      count++;
-    }
-  return (count);
+size_t count = 0;
+while (h != NULL)
+{
+h = h->next;
+count++;
+}
+return (count);
 }
