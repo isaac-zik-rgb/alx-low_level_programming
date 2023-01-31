a53962d2ec4eb051345799221943709fa6763ccb
isaac castro <isaacokechukwu200021@gmail.com>
2023-01-31 12:01:21 -0800

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,20 +10,20 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *temp;
-  listint_t *ptr = *head;
-  temp = malloc(sizeof(listint_t));
-  if (temp == NULL)
-    return (NULL);
-  temp->n = n;
-  temp->next = NULL;
-  if (*head == NULL)
-    {
-      *head = temp;
-      return (temp);
-    }
-  while (ptr->next)
-    ptr = ptr->next;
-  ptr->next = temp;
-  return (temp);
+listint_t *temp;
+listint_t *ptr = *head;
+temp = malloc(sizeof(listint_t));
+if (temp == NULL)
+return (NULL);
+temp->n = n;
+temp->next = NULL;
+if (*head == NULL)
+{
+*head = temp;
+return (temp);
+}
+while (ptr->next)
+ptr = ptr->next;
+ptr->next = temp;
+return (temp);
 }
