ba6f67f08700749babdbad5f551695f29ce4833b
isaac castro <isaacokechukwu200021@gmail.com>
2023-02-01 17:43:47 -0800

    task101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint_safe - print the elements of the list node
+ * @head: a pointer to the first node of the list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nodes, index = 0;
+nodes = looped_listint_len(head);
+if (nodes == 0)
+{
+for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (nodes);
+}
