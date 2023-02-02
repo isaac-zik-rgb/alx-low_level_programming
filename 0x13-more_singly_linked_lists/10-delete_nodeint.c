1df6f498a9742c128c1e64d3cc909ff0d8d67002
isaac castro <isaacokechukwu200021@gmail.com>
2023-02-01 16:35:38 -0800

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,26 +10,26 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  listint_t *temp = *head;
-  listint_t *current = NULL;
-  unsigned int n = 0;
-  if (*head == NULL)
-    return (-1);
-  if (index == 0)
-    {
-      *head = (*head)->next;
-      free(temp);
-      return (1);
-    }
-  while (n < index - 1)
-    {
-      if (!temp || !(temp->next))
-	return (-1);
-      temp = temp->next;
-      n++;
-    }
-  current = temp->next;
-  temp->next = current->next;
-  free(current);
-  return (1);
+listint_t *temp = *head;
+listint_t *current = NULL;
+unsigned int n = 0;
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+*head = (*head)->next;
+free(temp);
+return (1);
+}
+while (n < index - 1)
+{
+if (!temp || !(temp->next))
+return (-1);
+temp = temp->next;
+n++;
+}
+current = temp->next;
+temp->next = current->next;
+free(current);
+return (1);
 }
