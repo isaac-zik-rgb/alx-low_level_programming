aa4919f955c82f1a031bfb085231f80c0d5c375c
isaac castro <isaacokechukwu200021@gmail.com>
2023-02-04 09:43:44 -0800

    seting

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * binary_to_unit - convert binary number to an unsigned integer
  * @b: a pointer to a string of 0s and 1s
@@ -8,7 +8,7 @@
  */
 unsigned int binary_to_unit(const char *b)
 {
-int i;
+unsigned int i;
 unsigned int numb = 0;
 if (!b)
 return (0);
