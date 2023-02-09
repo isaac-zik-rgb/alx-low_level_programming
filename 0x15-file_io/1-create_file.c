f48ac5f4639c0648c00ba1f0d7a27b2d4d26f294
isaac castro <isaacokechukwu200021@gmail.com>
2023-02-09 14:42:40 -0800

    check

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: a pointer to the name of file to be created
+ * @text_content: a pointer to the string to write on the file
+ *
+ * Return: if function fail --1, otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+  int fd, w, len = 0;
+  if (filename == NULL)
+    return (-1);
+  if (text_content != NULL)
+    {
+      for (len = 0; text_content[len]; len++)
+	}
+  fd = open(filename, O_CRATE | O_RDWR | O_TRUNC, 0600);
+  w = write(fd, text_content, len);
+  if (fd == -1 || w == -1)
+    return (-1);
+  close(fd);
+  return (1);
+}
