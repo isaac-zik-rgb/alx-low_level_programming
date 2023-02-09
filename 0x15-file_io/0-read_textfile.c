e7d9434132b6279bad1fdca588740d2255bbe59b
isaac castro <isaacokechukwu200021@gmail.com>
2023-02-09 09:30:58 -0800

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * read_textfile - read textfile and print it to the posix standard out-put
+ * read_textfile - read textfile and print it to prosix stdout
  * @filename: a pointer to the file to read
  * @letters: numbers of letters to be read
- * @Return: w - actual number of bytes to be read and printed
- * 0 wgen funtion failed or filename is NULL
+ * Return: w-actual  number of bytes to be read and printed 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,12 +13,12 @@ char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
-fd = open(filename,O_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
-t = read(fd,buf,letters);
-w = write(STDOUT_FILENO,buf,t);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
 free(buf);
 close(fd);
 return (w);
