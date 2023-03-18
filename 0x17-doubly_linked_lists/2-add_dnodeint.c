b5abe053b4d64abf684969b693c15a1214fd2cec
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-18 16:29:49 -0700

    done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
   temp->n = n;
   temp->next = NULL;
   temp->prev = NULL;
-
+  temp->next = *head;
   if (*head != NULL)
     {
       (*head)->prev = temp;
