c16b8bfcfc0bd09f70f7bbc016860a4926898c5e
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-20 19:26:26 -0700

    fixed

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * add - add two inger numbers
+ * @a: the firt parameter
+ * @b: the second parameter
+ * Return: the sum of the two numbers
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - subtract two given numbers
+ * @a: the first parameter
+ * @b: second parameter
+ * Return: the subraction of the both numbers
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * div - divide two integer numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the remainer of the numbers
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mul - mutiplies two integer numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the multiplication of the given numbers
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * mod - return the modulus of two numbers
+ * @a: first parameter
+ * @b: the second parameter
+ * Return: either true or false
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
