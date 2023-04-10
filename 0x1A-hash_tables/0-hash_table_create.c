8bd71a990f1909c1f084b9a8c95701ae69eca6a8
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-10 08:50:12 +0000

    fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,25 +5,23 @@
 /**
  * hash_table_create - create a hash table
  * @size: the size of the hash table to be created
- * Return: Null if something go wrong or pointer to newly created 
-table
-*/
-
+ * Return: a pointer to the table or Null if failed
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *Ht;
-  unsigned int i;
-  Ht = malloc(sizeof(hash_table_t));
-  if (Ht == NULL)
-    return NULL;
-  Ht->size = size;
+hash_table_t *Ht;
+unsigned int i;
+Ht = malloc(sizeof(hash_table_t));
+if (Ht == NULL)
+return (NULL);
+Ht->size = size;
 
-  Ht->array = malloc(sizeof(hash_table_t *) * size);
+Ht->array = malloc(sizeof(hash_table_t *) * size);
 
-  if (Ht->array == NULL)
-    return NULL;
+if (Ht->array == NULL)
+return (NULL);
 
-  for (i = 0; i < Ht->size; i++)
-    Ht->array[i] = NULL;
-  return (Ht);
+for (i = 0; i < Ht->size; i++)
+Ht->array[i] = NULL;
+return (Ht);
 }
